bf58acecbd3c953f670da427f3857f43ac803e64
ramarao4507 <ramarao4507@gmail.com>
2017-02-28 17:33:15 +0530

    Add files via upload

---
+++ day_5/Rectangle Area Perimeter.py
@@ -0,0 +1,24 @@
+
+#from math import sqrt
+def Rectangle(width, length):
+ 
+    
+    Area = width * length
+ 
+    
+    Perimeter = 2 * (width + length)
+
+   
+ 
+    print "Area of a Rectangle is:    %.2f" %Area
+    print "Perimeter of Rectangle is: %.2f" %Perimeter
+    #print "Diagonal Length of Rectangle is: .2f" %d
+
+a = int(raw_input("Enter Width of The Rectangle :"))
+b = int(raw_input("Enter length of The Rectangle:"))
+Rectangle(a, b)
+
+
+
+
+
